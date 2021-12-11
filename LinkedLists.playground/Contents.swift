ec6dc08a855cd0d3c38c7f487cc7a6ca76baba8b
raxavelo <raxavelo@gmail.com>
2021-12-11 19:57:02 +0100

    Added Node.swift

--- LinkedLists.playground/Contents.swift
+++ LinkedLists.playground/Contents.swift
@@ -1,3 +1,12 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+example(of: "creating and linking nodes") {
+  let node1 = Node(value: 1)
+  let node2 = Node(value: 2)
+  let node3 = Node(value: 3)
+  
+  node1.next = node2
+  node2.next = node3
+  
+  print(node1)
+}
